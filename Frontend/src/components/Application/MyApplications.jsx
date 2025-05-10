98323d31487b26fba2a7aa50252295c16eef9a47
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2025-05-11 02:45:08 +0530

    Update MyApplications.jsx

--- Frontend/src/components/Application/MyApplications.jsx
+++ Frontend/src/components/Application/MyApplications.jsx
@@ -18,7 +18,7 @@ const MyApplications = () => {
     try {
       if (user && user.role === "Employeer") {
         axios
-          .get("http://localhost:5000/api/v1/application/employer/getall", {
+          .get("https://jobsphere-mern-stack-job-portal-backend.onrender.com/api/v1/application/employer/getall", {
             withCredentials: true,
           })
           .then((res) => {
@@ -26,7 +26,7 @@ const MyApplications = () => {
           });
       } else {
         axios
-          .get("http://localhost:5000/api/v1/application/jobseeker/getall", {
+          .get("https://jobsphere-mern-stack-job-portal-backend.onrender.com/api/v1/application/jobseeker/getall", {
             withCredentials: true,
           })
           .then((res) => {
@@ -47,7 +47,7 @@ const MyApplications = () => {
   const deleteApplication = (id) => {
     try {
       axios
-        .delete(`http://localhost:5000/api/v1/application/delete/${id}`, {
+        .delete(`https://jobsphere-mern-stack-job-portal-backend.onrender.com/api/v1/application/delete/${id}`, {
           withCredentials: true,
         })
         .then((res) => {
